7c4cfcce4e7c360d5665b95094633ea51ba74919
koniz-dev <kietna.work@gmail.com>
2025-12-07 10:42:41 +0700

    chore: Update Java toolchain version in build configuration
    - Set the Java language version to 17 in the build.gradle.kts file to ensure compatibility with modern Java features.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,6 +5,12 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
 android {
     namespace = "com.example.flutter_starter"
     compileSdk = flutter.compileSdkVersion
